6e83edbf688452e5fb32a5a04b9d0205c7ec60f8
DEADushka108 <59721081+DEADushka108@users.noreply.github.com>
2020-02-17 16:46:42 +0500

    Слайдер и модальное окно
    добавил включение и выключение модального окна и слайдера на главной странице

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Модальное окно */
+
+function toggleHidden (btn, section) {
+  btn.addEventListener('click', function (btn) {
+    btn.preventDefault();
+    section.classList.toggle('modal-hidden');
+  });
+}
+
+let btnPopUp = document.querySelector('.btn-popup');
+let btnWriteUsClose = document.querySelector('.btn-close');
+let writeUs = document.querySelector('.write-us');
+
+toggleHidden(btnPopUp, writeUs);
+toggleHidden(btnWriteUsClose, writeUs);
+
+/*Переключатель слайдера*/ 
+
+function toggleSlides (toggles, slides) {
+  for (let i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('change', function() {
+      for (let j = 0; j < slides.length; j++) {
+        slides[j].classList.remove('active');
+        slides[i].classList.add('active');
+        }
+      });
+  }
+}
+
+let slider = document.querySelector('.slide-list');
+let totalSlides = slider.children;
+let totalToggles = document.querySelectorAll('.slider-toggle');
+
+toggleSlides(totalToggles, totalSlides);
