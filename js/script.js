513522592fe0f8d97d5c264e28ef141f6c42fb0e
DEADushka108 <59721081+DEADushka108@users.noreply.github.com>
2020-02-22 12:44:03 +0500

    Анимация и ПП
    Добавил анимацию появления модального окна, и сделал ПП модального окна.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-let btnPopUp = document.querySelector('.btn-popup');
-let btnWriteUsClose = document.querySelector('.btn-close');
-let writeUs = document.querySelector('.write-us');
-let slider = document.querySelector('.slide-list');
+
+var btnPopUp = document.querySelector(".btn-popup");
+var btnWriteUsClose = document.querySelector(".btn-close");
+var writeUs = document.querySelector(".write-us");
+
+var form = writeUs.querySelector("form");
+var userName = writeUs.querySelector("[name=name]");
+var email = writeUs.querySelector("[name=email]");
+  
+var isStorageSupport = true;
+var storage = "";
+
+let slider = document.querySelector(".slide-list");
 let totalSlides = slider.children;
-let totalToggles = document.querySelectorAll('.slider-toggle');
-
-/* Модальное окно */
-
-function showPopup (btn, section) {
-  btn.addEventListener('click', function (btn) {
-    btn.preventDefault();
-    section.classList.toggle('modal-show');
-  });
-}
+let totalToggles = document.querySelectorAll(".slider-toggle");
 
 /*Переключатель слайдера*/ 
 
@@ -29,6 +29,51 @@ function toggleSlides (toggles, slides) {
   }
 }
 
-showPopup(btnPopUp, writeUs);
-showPopup(btnWriteUsClose, writeUs);
 toggleSlides(totalToggles, totalSlides);
+
+try {
+  storage = localStorage.getItem("userName");
+} catch (err) {
+  isStorageSupport = false;
+}
+  
+btnPopUp.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUs.classList.add("modal-show");
+    
+  if (storage) {
+    userName.value = storage;
+    email.focus();
+  } else {
+    userName.focus();
+    }
+});
+  
+btnWriteUsClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  writeUs.classList.remove("modal-show");
+  writeUs.classList.remove("modal-error");
+});
+  
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !email.value) {
+    evt.preventDefault();
+    writeUs.classList.remove("modal-error");
+    writeUs.offsetWidth = writeUs.offsetWidth;
+    writeUs.classList.add("modal-error");
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("userName", userName.value);
+      }
+    }
+});
+  
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (writeUs.classList.contains("modal-show")) {
+      writeUs.classList.remove("modal-show");
+      writeUs.classList.remove("modal-error");
+      }
+    }
+});
